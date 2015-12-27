f465a91565eb566900dd7eea5a284e16c3cb5513
unknown <tyler.niu@mail.utoronto.ca>
2015-12-27 08:02:54 +0800

    resouce page working

--- frontend.py
+++ frontend.py
@@ -7,7 +7,7 @@ import time
 
 
 
-
+'''
 #using python flask framework to host our backend
 app = Flask(__name__, template_folder='C:\\Users\\Redirection\\NIUC3\\Desktop\\Archives\\Tyler\\angular' , static_url_path='')
 cors = CORS(app)
@@ -31,18 +31,17 @@ def send_assets(path):
 @app.route('/app/tpls/<path:path>')
 def send_tpls(path):
 	return send_from_directory('C:\\Users\\Redirection\\NIUC3\\Desktop\\Archives\\Tyler\\angular\\app\\tpls', path)
-
-
-
-
-
-
-
-
-
 '''
+
+
+
+
+
+
+
+
 #using python flask framework to host our backend
-app = Flask(__name__, template_folder='C:\\Users\\tyler1314371\\Desktop\\game\\HungerCraft\\angular' , static_url_path='')
+app = Flask(__name__, template_folder='E:\\Hungercraft\\HungerCraft\\angular' , static_url_path='')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -55,17 +54,17 @@ def index():
 
 @app.route('/app/js/<path:path>')
 def send_js(path):
-	return send_from_directory('C:\\Users\\tyler1314371\\Desktop\\game\\HungerCraft\\angular\\app\\js', path)
+	return send_from_directory('E:\\Hungercraft\\HungerCraft\\angular\\app\\js', path)
 
 @app.route('/assets/<path:path>')
 def send_assets(path):
-	return send_from_directory('C:\\Users\\tyler1314371\\Desktop\\game\\HungerCraft\\angular\\assets', path)
+	return send_from_directory('E:\\Hungercraft\\HungerCraft\\angular\\assets', path)
 
 @app.route('/app/tpls/<path:path>')
 def send_tpls(path):
-	return send_from_directory('C:\\Users\\tyler1314371\\Desktop\\game\\HungerCraft\\angular\\app\\tpls', path)
+	return send_from_directory('E:\\Hungercraft\\HungerCraft\\angular\\app\\tpls', path)
 
-'''
+
 
 
 
