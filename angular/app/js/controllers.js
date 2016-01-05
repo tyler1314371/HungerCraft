d5b7e74f9848f60f9cb85e2d8173898cfcafa01e
unknown <NIUC3@TDL00884531.TDBFG.com>
2016-01-05 13:14:40 -0500

    rotating planet

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -566,7 +566,7 @@ angular.module('xenon.controllers', []).
 				 for (var skill in ships[key]['skills']){
 				 	ships[key]['skills'][skill]['unlocked'] = 1;
 				 }
-				 
+
 				}
 			for (var key in raids) {
 				 raids[key]["unlocked"] = 1;
@@ -736,15 +736,15 @@ angular.module('xenon.controllers', []).
 
 		    };
 		};
-	
+
 		$scope.EnterInstance = function(instance_name){
-		    
+
 			if(raids[instance_name]['unlocked']!=1){
 
 				toastr.error("Instance is Locked"
 						, opts);
 				return false;
-				
+
 		    }else{
 
 		    	if(raids[instance_name]['timer']!="00:00:00"){
@@ -752,7 +752,7 @@ angular.module('xenon.controllers', []).
 				toastr.error("Instance is on cooldown"
 						, opts);
 				return false;
-					
+
 			    }
 
 
@@ -763,17 +763,17 @@ angular.module('xenon.controllers', []).
 			    $scope.openModal('research', 'lg');
 		    }
 
-		    
+
 		};
 
 		$scope.RefreshRaidCD = function(instance_name){
-		    
+
 			if(raids[instance_name]['unlocked']!=1){
 
 				toastr.error("Instance is Locked"
 						, opts);
 				return false;
-				
+
 		    }else{
 
 		    	if(dark_matter['current_owned']<2){
@@ -781,7 +781,7 @@ angular.module('xenon.controllers', []).
 				toastr.error("Not enough dark matter"
 						, opts);
 				return false;
-					
+
 			    }
 
 
@@ -790,7 +790,7 @@ angular.module('xenon.controllers', []).
 				toastr.error(instance_name+" is already ready"
 						, opts);
 				return false;
-					
+
 			    }
 
 
@@ -800,11 +800,11 @@ angular.module('xenon.controllers', []).
 						, opts);
 
 			    	raids[instance_name]['timer']= "00:00:00";
-			    
+
 
 		    }
 
-		    
+
 		};
 
 
@@ -813,13 +813,13 @@ angular.module('xenon.controllers', []).
 
 
 		$scope.BuildShip = function(ship_name, amount){
-		    
+
 			if(ships[ship_name]['unlocked']!=1){
 
 				toastr.error("Ship is Locked"
 						, opts);
 				return false;
-				
+
 		    }else{
 
 		    	var metal_cost;
@@ -862,14 +862,14 @@ angular.module('xenon.controllers', []).
 				toastr.error("Not enough metal"
 						, opts);
 				return false;
-					
+
 			    }
 			    if(crystal_cost>crystal.current_owned){
 
 				toastr.error("Not enough crystal"
 						, opts);
 				return false;
-					
+
 			    }
 
 			    metal.current_owned -= metal_cost;
@@ -880,13 +880,13 @@ angular.module('xenon.controllers', []).
 				return false;
 		    }
 
-		    
+
 		};
 
 
 
 		$scope.PaypalCheckout = function(){
-		    
+
 			var amount_dm = 0;
 			var amount_CAD = 0;
 
@@ -908,7 +908,7 @@ angular.module('xenon.controllers', []).
 				amount_CAD += 5.99;
 			}
 
-		    
+
 			console.log(amount_dm);
 			console.log(amount_CAD);
 
@@ -942,7 +942,7 @@ angular.module('xenon.controllers', []).
 
 			update_state = window.setInterval(function(){
 
-		
+
 			metal.income = Math.floor(metal.income_base  * metal.current_mine_level * Math.pow(1.1,metal.current_mine_level));
 			crystal.income = Math.floor(crystal.income_base  * crystal.current_mine_level * Math.pow(1.1,crystal.current_mine_level));
 
@@ -962,14 +962,14 @@ angular.module('xenon.controllers', []).
 				var minute = Number(current_time[1]);
 				var second = Number(current_time[2]);
 
-				
+
 
 				if(second == 0 && minute ==0 && hour ==0){
 					continue;
 				}
 
 				second -= 1;
-				
+
 				if(second < 0){
 					second = 0;
 					minute -=1;
@@ -1010,7 +1010,7 @@ angular.module('xenon.controllers', []).
 				raids[key]["timer"] = hour+":"+minute+":"+second
 
 			}
-		
+
 
 
 
@@ -1029,7 +1029,7 @@ angular.module('xenon.controllers', []).
 
 
 
-			
+
 
 
 
@@ -1414,7 +1414,7 @@ angular.module('xenon.controllers', []).
 		$scope.NextLevel = function(){
 
 
-			
+
 
 
 			var level_metal_req_temp = Math.floor(level.level_metal_req_ori * Math.pow(1.5,level.current_level));
@@ -1450,6 +1450,117 @@ angular.module('xenon.controllers', []).
 
 
 		};
+
+
+
+		(function() {
+  var globe = planetaryjs.planet();
+  // Load our custom `autorotate` plugin; see below.
+  globe.loadPlugin(autorotate(10));
+  // The `earth` plugin draws the oceans and the land; it's actually
+  // a combination of several separate built-in plugins.
+  //
+  // Note that we're loading a special TopoJSON file
+  // (world-110m-withlakes.json) so we can render lakes.
+  globe.loadPlugin(planetaryjs.plugins.earth({
+    topojson: { file:   'assets/world-110m-withlakes.json' },
+    oceans:   { fill:   '#000080' },
+    land:     { fill:   '#339966' },
+    borders:  { stroke: '#008000' }
+  }));
+  // Load our custom `lakes` plugin to draw lakes; see below.
+  globe.loadPlugin(lakes({
+    fill: '#000080'
+  }));
+  // The `pings` plugin draws animated pings on the globe.
+  	//globe.loadPlugin(planetaryjs.plugins.pings());
+  // The `zoom` and `drag` plugins enable
+  // manipulating the globe with the mouse.
+  globe.loadPlugin(planetaryjs.plugins.zoom({
+    scaleExtent: [50, 300]
+  }));
+  globe.loadPlugin(planetaryjs.plugins.drag({
+    // Dragging the globe should pause the
+    // automatic rotation until we release the mouse.
+    onDragStart: function() {
+      this.plugins.autorotate.pause();
+    },
+    onDragEnd: function() {
+      this.plugins.autorotate.resume();
+    }
+  }));
+  // Set up the globe's initial scale, offset, and rotation.
+  globe.projection.scale(50).translate([50, 50]).rotate([0, -10, 0]);
+
+  var canvas = document.getElementById('rotatingGlobe');
+  // Special code to handle high-density displays (e.g. retina, some phones)
+  // In the future, Planetary.js will handle this by itself (or via a plugin).
+  if (window.devicePixelRatio == 2) {
+    canvas.width = 300;
+    canvas.height = 300;
+    context = canvas.getContext('2d');
+    context.scale(2, 2);
+  }
+  // Draw that globe!
+  globe.draw(canvas);
+
+  // This plugin will automatically rotate the globe around its vertical
+  // axis a configured number of degrees every second.
+  function autorotate(degPerSec) {
+    // Planetary.js plugins are functions that take a `planet` instance
+    // as an argument...
+    return function(planet) {
+      var lastTick = null;
+      var paused = false;
+      planet.plugins.autorotate = {
+        pause:  function() { paused = true;  },
+        resume: function() { paused = false; }
+      };
+      // ...and configure hooks into certain pieces of its lifecycle.
+      planet.onDraw(function() {
+        if (paused || !lastTick) {
+          lastTick = new Date();
+        } else {
+          var now = new Date();
+          var delta = now - lastTick;
+          // This plugin uses the built-in projection (provided by D3)
+          // to rotate the globe each time we draw it.
+          var rotation = planet.projection.rotate();
+          rotation[0] += degPerSec * delta / 1000;
+          if (rotation[0] >= 180) rotation[0] -= 360;
+          planet.projection.rotate(rotation);
+          lastTick = now;
+        }
+      });
+    };
+  };
+
+  // This plugin takes lake data from the special
+  // TopoJSON we're loading and draws them on the map.
+  function lakes(options) {
+    options = options || {};
+    var lakes = null;
+
+    return function(planet) {
+      planet.onInit(function() {
+        // We can access the data loaded from the TopoJSON plugin
+        // on its namespace on `planet.plugins`. We're loading a custom
+        // TopoJSON file with an object called "ne_110m_lakes".
+        var world = planet.plugins.topojson.world;
+        lakes = topojson.feature(world, world.objects.ne_110m_lakes);
+      });
+
+      planet.onDraw(function() {
+        planet.withSavedContext(function(context) {
+          context.beginPath();
+          planet.path.context(context)(lakes);
+          context.fillStyle = options.fill || 'black';
+          context.fill();
+        });
+      });
+    };
+  };
+})();
 
 
 	}).
